6b3429409d0bffb55aabd5729bfa0e005901e08e
Tahmid79274 <tahmidur.rahman.79274@gmail.com>
2025-09-25 01:51:37 +0600

    need to work more

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from stats import get_letter_counts
+from stats import get_sorted_report, get_letter_counts
+import sys
 
 def main():
-    #get_num_words(get_book_text("books/frankenstein.txt"))
-    #print(get_letter_counts())
-    result = get_letter_counts()
-    print(result)
+    if len(sys.argv) != 2:
+        print(sys.argv)
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    # print(sys.argv)
+    get_sorted_report(get_letter_counts(book_path),book_path)
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 
+
 def get_book_text(filePath):
     with open(filePath) as f:
         #file_contents = f.read()
@@ -9,8 +10,8 @@ def get_num_words(text):
     words = text.split()
     print(f"Found {len(words)} total words")
 
-def get_letter_counts():
-    words = get_book_text("books/frankenstein.txt").lower()
+def get_letter_counts(path):
+    words = get_book_text(path).lower()
     # print(words)
     track_letters = {}
     for letter in range(0,len(words)):
@@ -20,3 +21,25 @@ def get_letter_counts():
         else:
             track_letters[words[letter]] = 1
     return track_letters
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_report(letter_counts,path):
+    dictionary_list = []
+    print("============ BOOKBOT ============\n----------- Word Count ----------")
+    get_num_words(get_book_text(path))
+    print("--------- Character Count -------")
+    for key in letter_counts.keys():
+        dictionary_list.append({
+            "char": key,
+            "num": letter_counts[key]
+        })
+    # print(dictionary_list)
+    dictionary_list.sort(reverse= True, key=sort_on)
+    # print("=========================================")
+    # print(dictionary_list)
+    for dictionary in dictionary_list:
+        if dictionary["char"].isalpha():
+            print(f"{dictionary["char"]}: {dictionary["num"]}")
+    print("============= END ===============")
